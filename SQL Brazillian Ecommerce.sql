4e17a7ea093c061f182ec20a6ec8a99da73785af
thierno balde <dev.balde17@gmail.com>
2023-04-28 09:23:36 -0400

    New tables
    Creating tables customers, geolocation, order_items and order_payments

--- SQL Brazillian Ecommerce.sql
+++ SQL Brazillian Ecommerce.sql
@@ -46,5 +46,63 @@ CREATE TABLE Products (product_id VARCHAR(255) NOT NULL,
    review_creation_date DATETIME NOT NULL,
    review_answer_timestamp DATETIME NOT NULL);
    
-    SELECT *FROM Order_Reviews;
-   
+ SELECT *FROM Order_Reviews;
+
+  /**Category customers Table**/  
+CREATE TABLE `customers` (
+  `customer_id` VARCHAR(255) NOT NULL,
+  `customer_unique_id` VARCHAR(255) NOT NULL,
+  `customer_zip_code_prefix` INT(11) NOT NULL,
+  `customer_city` VARCHAR(255) NOT NULL,
+  `customer_state` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+);
+
+SELECT *FROM customers;
+
+  /**Category geolocation Table**/
+CREATE TABLE `geolocation` (
+  `order_id` VARCHAR(255) NOT NULL,
+  `order_item_id` INT(11) NOT NULL,
+  `product_id` VARCHAR(255) NOT NULL,
+  `seller_id` VARCHAR(255) NOT NULL,
+  `shipping_limit_date` DATETIME NOT NULL,
+  `price` DOUBLE(10,2) NOT NULL,
+  `freight_value` DOUBLE(10,2) NOT NULL,
+  PRIMARY KEY (`order_id`, )
+);
+
+
+SELECT *FROM geolocation;
+
+
+  /**Category order_payments Table**/  
+
+CREATE TABLE `order_payments` (
+  `order_id` VARCHAR(255) NOT NULL,
+  `payment_sequential` INT(11) NOT NULL,
+  `payment_type` VARCHAR(255) NOT NULL,
+  `payment_installments` INT(11) NOT NULL,
+  `payment_value` DOUBLE(10,2) NOT NULL,
+  PRIMARY KEY (`order_id`)
+);
+
+
+SELECT *FROM order_payments;
+
+
+
+  /**Category order_items Table**/  
+CREATE TABLE `order_items` (
+  `order_id` VARCHAR(255) NOT NULL,
+  `order_item_id` INT(11) NOT NULL,
+  `product_id` VARCHAR(255) NOT NULL,
+  `seller_id` VARCHAR(255) NOT NULL,
+  `shipping_limit_date` DATETIME NOT NULL,
+  `price` DOUBLE(10,2) NOT NULL,
+  `freight_value` DOUBLE(10,2) NOT NULL,
+  PRIMARY KEY (`order_id`)
+);
+
+
+SELECT *FROM order_items;
